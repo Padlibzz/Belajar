e91a78390037cea9327a5149a55ad75be599c180
Padlibzz <padlibadzara@gmail.com>
2025-10-26 17:41:01 +0700

    tambah opsi login

--- tugas-dasprob/ATMsimple.java
+++ tugas-dasprob/ATMsimple.java
@@ -2,165 +2,220 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class ATMsimple{
-	public static void main(String[] args) {
-		logikaATM.menu();
+public class SimpleATM {
+    ;
+    public static Scanner input = new Scanner(System.in);
 
-	}
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("+--------------------------------------------------------+");
+            System.out.println("|                                                        |");
+            System.out.println("|             [ ] BANK DIGITAL KELOMPOK 3 [ ]            |");
+            System.out.println("|________________________________________________________|");
+            System.out.println("|                                                        |");
+            System.out.println("|           **SELAMAT DATANG DI ATM SEDERHANA**          |");
+            System.out.println("|                                                        |");
+            System.out.println("|========================================================|");
+            System.out.println("|                             | ______ |       |         |");
+            System.out.println("| 1. login                    |[      ]| [   ] |         |");
+            System.out.println("| 2. singin                   |[______]| [   ] |         |");
+            System.out.println("| 3. exit                     | [123]  | [___] |         |");
+            System.out.println("+--------------------------------------------------------+");
+            int pilihan = input.nextInt();
+            input.nextLine();
+            if (pilihan == 1) {
+                atm.login();
+                atm.menu();
+            } else if (pilihan == 2) {
+                atm.adduser();
+                continue;
+            } else if (pilihan == 3) {
+                break;
+            } else {
+                System.out.println("masukan nomber valid");
+                continue;
+            }
+        }
+
+    }
 }
 
-class logikaATM{
-	private static Scanner input = new Scanner(System.in);
-	private static ArrayList<String> user = new ArrayList<>(Arrays.asList("padli","risca","apif"));
-    private static ArrayList<Integer> pin = new ArrayList<>(Arrays.asList(111,222,333));
-    private static ArrayList<Long> saldo = new ArrayList<>(Arrays.asList(1000000L,2000000L,3000000L));
-	private static int userid;
-	private static int sandi;
+class atm {
+    private static ArrayList<String> user = new ArrayList<>(Arrays.asList("padli", "risca", "apif"));
+    private static ArrayList<Integer> pin = new ArrayList<>(Arrays.asList(111, 222, 333));
+    private static ArrayList<Long> saldo = new ArrayList<>(Arrays.asList(1000000L, 2000000L, 3000000L));
+    private static int userid;
 
-	public static void tambahuser(){
-		System.out.print("masukan nama : ");
-		String newuser = input.nextLine();
-		System.out.print("buat sandi pin : ");
-		int newpin = input.nextInt();
-		long newsaldo = 0;
-		user.add(newuser);
-		pin.add(newpin);
-		saldo.add(newsaldo);
-	}
+    public static Scanner input = new Scanner(System.in);
 
-	public static int autentikasiuser(String nama){
-		for (int i = 0; i < user.size(); i++) {
-			if (nama.equals(user.get(i))) {
-				return i;
-			}
-		}
-		return -1;
-	}
+    public static void menu() {
+        while (true) {
+            System.out.println("+--------------------------------------------------------+");
+            System.out.println("|                                                        |");
+            System.out.println("|             [ ] BANK DIGITAL KELOMPOK 3 [ ]            |");
+            System.out.println("|________________________________________________________|");
+            System.out.println("|                                                        |");
+            System.out.println("|           **SELAMAT DATANG DI ATM SEDERHANA**          |");
+            System.out.println("|                                                        |");
+            System.out.println("|========================================================|");
+            System.out.println("|                             | ______ |       |         |");
+            System.out.println("| 1. Cek Saldo                |[      ]| [   ] |         |");
+            System.out.println("| 2. Setor Tunai              |[______]| [   ] |         |");
+            System.out.println("| 3. Tarik Tunai              | [123]  | [___] |         |");
+            System.out.println("| 4. Kirim Tunai              | [456]  | [   ] |         |"); 
+            System.out.println("| 5. Buat Akun                | [789]  | [   ] |         |");
+            System.out.println("| 6. **EXIT**                 | [ *#]  |       |         |");
+            System.out.println("+--------------------------------------------------------+");
 
-	public static boolean autentikasipin(int userid,int sandi){
-		return sandi == pin.get(userid);
-	}
+            int pilihan = input.nextInt();
+            input.nextLine();
+            if (pilihan == 6) {
+                break;
+            } else if (pilihan == 1) {
+                ceksaldo();
+            } else if (pilihan == 2) {
+                depo();
+            } else if (pilihan == 3) {
+                withdraw();
+            } else if (pilihan == 4) {
+                transfer();
+            } else if (pilihan == 5) {
+                adduser();
+            }
+        }
 
-	public static void cekuser(){
-		System.out.print("masukan nama : ");
-		String userinput = input.nextLine();
-		userid = autentikasiuser(userinput);
-		if (userid != -1) {
-			System.out.println("--------------------------------");
-			System.out.println("Masukan Sandi");
-			sandi = input.nextInt();
-			if (autentikasipin(userid,sandi)) {
-				System.out.println("--------------------------------");
-            	System.out.println("Selamat datang, " + user.get(userid));
-			}else{
-				System.out.println("sandi salah");
-			}
-		}else{
-			System.out.println("user tidak ada");
-		}
+    }
 
-	}
-	public static void ceksaldo(){
-		cekuser();
-		if (autentikasipin(userid,sandi)) {
-			System.out.println("saldo anda adalah : " + saldo.get(userid));
-		}
-	}
-	public static void setortunai(){
-		cekuser();
-		System.out.print("Masukan jumlah yang mau di setor : ");
-		long setor = input.nextLong();
-		if (setor < 0) {
-			System.out.println("masukan nominal yang benar");
-		}else{
-			long saldobaru = saldo.get(userid) + setor;
-			saldo.set(userid, saldobaru);
-			System.out.println("saldo anda jadi : " + saldo.get(userid));
-		}
-		
-	}
+    public static int autentikasiuser(String username) {
+        for (int i = 0; i < user.size(); i++) {
+            if (username.equals(user.get(i))) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	public static void tariktunai(){
-		cekuser();
-		System.out.print("Masukan jumlah yang mau di tarik : ");
-		long tarik = input.nextLong();
-		if (tarik < 0 || tarik > saldo.get(userid)) {
-			System.out.println("masukan nominal yang benar");
-		}else{
-			long tarikbaru = saldo.get(userid) - tarik;
-			saldo.set(userid, tarikbaru);
-			System.out.println("saldo anda jadi : " + saldo.get(userid));
-		}
-	}
+    public static boolean autentikasipin(int userpin) {
 
-	public static void kirimtunai(){
-		cekuser();
-		System.out.print("mau kirim kemana : ");
-		input.nextLine();
-		String penerimainput = input.nextLine();
-		System.out.println();
-		int penerima = autentikasiuser(penerimainput);
-		
-		if(penerima == userid) {
-			System.out.println("Tidak bisa kirim ke diri sendiri");
-			return;
-		}
-		if(penerima == -1){
-			System.out.println("user tidak ada");
-			return;
-		}
-		if(penerima != -1 && penerima != userid){
-			System.out.print("Jumlah yang di kirim : ");
-			long kirim = input.nextLong();
-			if (kirim < 0 || kirim > saldo.get(userid)) {
-				System.out.println("masukan nominal yang benar");
-			}else{
-				long saldok = saldo.get(userid) - kirim;
-				saldo.set(userid, saldok);
-				long saldom = saldo.get(penerima) + kirim;
-				saldo.set(penerima, saldom);
-				System.out.println("saldo anda jadi : " + saldo.get(userid));
-			}
-		}
-	}	
+        return userpin == pin.get(userid);
+    }
 
-	public static void menu() {
-		int pilihan = 0;
-		while(true){
-			System.out.println("  ________________________________________________________   ");
-            System.out.println("//========================================================\\ ");
-            System.out.println("||             [ ] BANK DIGITAL INDONESIA [ ]             || ");
-            System.out.println("||________________________________________________________|| ");
-            System.out.println("||                                                        || ");
-            System.out.println("||           **SELAMAT DATANG DI ATM SEDERHANA**          || ");
-            System.out.println("||                                                        || ");
-            System.out.println("||========================================================|| ");
-            System.out.println("|| 1. Cek Saldo                |[      ]| [   ] |         || ");
-            System.out.println("|| 2. Setor Tunai              |[______]| [   ] |         || ");
-            System.out.println("|| 3. Tarik Tunai              | [123]  | [___] |         || ");
-            System.out.println("|| 4. Kirim Tunai              | [456]  | [   ] |         || "); 
-            System.out.println("|| 5. Buat Akun                | [789]  | [   ] |         || ");
-            System.out.println("|| 6. **EXIT**                 | [ *#]  |       |         || ");
-            System.out.println("||========================================================|| ");
-            System.out.println("\\_______________________________________________________// ");
-            System.out.println(" \\_____________________________________________________//  ");
-			
-			pilihan = input.nextInt();
-			input.nextLine();
+    public static void login() {
+        while (true) {
+            System.out.print("Masukan nama : ");
+            String username = input.nextLine();
+            userid = autentikasiuser(username);
+            if (userid == -1) {
+                System.out.println("user tidak dapat ditemukan");
+                continue;
+            }else{
+                System.out.print("Masukan pin : ");
+                int userpin = input.nextInt();
+                input.nextLine();
+                if (autentikasipin(userpin)) {
+                    System.out.println("Halo " + user.get(userid));
+                    break;
+                }else{
+                    System.out.println("pin atau username salah");
+                    continue;
+                }
+            }
+        }
+    }
 
-			if (pilihan == 6){
-				break;	
-				}else if(pilihan == 1){
-					ceksaldo();
-				}else if(pilihan == 2){
-					setortunai();
-				}else if(pilihan == 3){
-					tariktunai();
-				}else if(pilihan == 4){
-					kirimtunai();
-				}else if(pilihan == 5){
-					tambahuser();
-				}					
-			}
-		}
-	}
+    public static void adduser() {
+        while (true) {
+            System.out.print("masukan nama : ");
+            String nama = input.nextLine();
+            int ceknama = autentikasiuser(nama);
+            if (ceknama == -1) {
+                System.out.print("buat sandi : ");
+                int sandi1 = input.nextInt();
+                input.nextLine();
+                System.out.print("konfirmasi sandi : ");
+                int sandi2 = input.nextInt();
+                input.nextLine();
+                if (sandi1 != sandi2) {
+                    System.out.println("sandi tidak sama");
+                    continue;
+                } else {
+                    user.add(nama);
+                    pin.add(sandi1);
+                    saldo.add(0L);
+                    System.out.println("user berhasil di buat");
+                    break;
+                }
+            } else {
+                System.out.println("nama sudah ada");
+            }
+        }
+    }
+
+    public static void ceksaldo() {
+        System.out.println("saldomu adalah : " + saldo.get(userid));
+
+    }
+
+    public static void depo() {
+        while (true) {
+            System.out.print("masukan nominal deposit : ");
+            long depo = input.nextLong();
+            if (depo <= 0) {
+                System.out.println("masukan nomor yang valid");
+                continue;
+            } else {
+                depo = saldo.get(userid) + depo;
+                saldo.set(userid, depo);
+                System.out.println("sukses");
+                break;
+            }    
+        }
+        
+    }
+
+    public static void withdraw() {
+        while (true) {
+            System.out.print("masukan nominal withdraw : ");
+            long withdraw = input.nextLong();
+            if (withdraw < 0 && withdraw > saldo.get(userid)) {
+                System.out.println("masukan nomor yang valid");
+                continue;
+            } else {
+                withdraw = saldo.get(userid) - withdraw;
+                saldo.set(userid, withdraw);
+                System.out.println("sukses");
+                break;
+            }    
+        }
+    }
+
+    public static void transfer() {
+        while (true) {
+            System.out.println("masukan nama penerima");
+            String cekpenerima = input.nextLine();
+            int penerima = autentikasiuser(cekpenerima);
+            if (penerima == -1) {
+                System.out.println("user tidak ada");
+                continue;
+            } else if (penerima == userid) {
+                System.out.println("tidak dapat mengirim ke diri sendiri");
+                continue;
+            } else {
+                System.out.print("masukan jumlah transfer : ");
+                long transfer = input.nextLong();
+                input.nextLine();
+                if (transfer < 0 || transfer > saldo.get(userid)) {
+                    System.out.println("masukan nomor yang valid");
+                    continue;
+                } else {
+                    long terima = saldo.get(penerima) + transfer;
+                    saldo.set(penerima, terima);
+                    long beri = saldo.get(userid) - transfer;
+                    saldo.set(userid, beri);
+                    break;
+                }
+            }    
+        }
+        
+    }
+}
